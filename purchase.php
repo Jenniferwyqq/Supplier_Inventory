591864d96bd464369737681343d9a73962511b8c
Jennifer Wu <71246844+Jenniferwyqq@users.noreply.github.com>
2021-10-22 00:00:00 -0700

    Update purchase.php

--- purchase.php
+++ purchase.php
@@ -23,86 +23,123 @@
             $result = $mysqli->query("SELECT shoe.id AS shoeid, box.id AS boxid, purchase_id AS dateid, employ.id AS empid, shoe.name AS style, shoe.color AS color, box.name AS box, purchase.unit_price AS unit_qrice, purchase.quantity AS quantity, purchase_date.date AS date, employ.id AS employ_id FROM shoe, box, purchase_date, purchase, employ WHERE purchase.shoe_id = shoe.id AND purchase.box_id = box.id AND purchase.purchase_id = purchase_date.id AND purchase.employ_id = employ.id;") or die($mysqli->error);
             //pre_r($result);
 			$result1 = $mysqli->query("SELECT DISTINCT shoe.name AS name FROM `shoe` WHERE 1;") or die($mysqli->error);
+			$result2 = $mysqli->query("SELECT DISTINCT shoe.color AS color FROM `shoe` WHERE 1;") or die($mysqli->error);
         ?>
 			
-			
+		
         
-            <div class="row justify-content-center">
-                <table class="table">
-                    <thead>
-                        <tr>
-							<th>STYLE</th>
-                            <th>COLOR</th>
-                            <th>MATERIAL</th>
-							<th>BOX</th>
-                            <th colspan="2">Action</th>
-                        </tr>
-                    </thead>
-                    <tr>
-                        <td> <select id="cmbMake" name="style">
-						<?php
-						while ($row = $result1->fetch_assoc()): ?>
-						    <option value="<?php echo $row['name']; ?>"><?php echo $row['name']; ?></option>
-						<?php endwhile; ?>  
-						</select></td>
+		<div class="row justify-content-center">
+			<table class="table">
+				<thead>
+					<tr>
+						<th>STYLE</th>
+						<th>COLOR</th>
+						<th>MATERIAL</th>
+						<th>BOX</th>
+						<th>QUANTITY</th>
+						<th colspan="2">Action</th>
+					</tr>
+				</thead>
+				<div class="row justify-content-center">
+					<tr>
+						<td>
+							<div>
+								<select id='name'>
+									<option>-----</option>
+									<?php
+									$result4 = $mysqli->query("SELECT DISTINCT shoe.name AS name FROM `shoe` WHERE 1;") or die($mysqli->error);
+									while($data=mysqli_fetch_assoc($result4)){
+									?>
+										  <option value="<?php echo $data['name'];?>"><?php echo $data['name'];?></option>
+									<?php
+									}
+									?>
+								</select>
+							</div>
+						</td>
+						<td>
+							<div>
+								<select id='color'>
+									<option>-----</option>
+								</select>
+							</div>
+						</td>
+						<td>
+							<div>
+								<select id='material'>
+									<option>-----</option>
+								</select>
+							</div>
+						</td>
+						<td>
+							<div>
+								<select id='box'>
+									<option>-----</option>
+									
+									<?php
+									$result5 = $mysqli->query("SELECT DISTINCT box.name FROM box, brand WHERE box.brand_id = 數字;") or die($mysqli->error);
+									while($data=mysqli_fetch_assoc($result5)){
+									?>
+										  <option value="<?php echo $data['name'];?>"><?php echo $data['name'];?></option>
+									<?php 
+									}
+									?>
+								</select>
+							</div>
+						</td>
+						<td>
+							<input type="text" name="name" value="" placeholder="Enter the quantity">
+						</td>
+						<td>
+							<button type="submit" class="btn btn-info" name="update">Update</button>   
+						</td>
+					</tr>
+				</div>
+			</table>
+		</div>
+		<input id = 'abc' type="text" style="font-size:12px" value="修改文字大小為 12px"><br>
+		<?php
+		
+		function pre_r( $array ) {
+			echo '<pre>';
+			print_r($array);
+			echo '</pre>';
+		}
+        ?>
+        
+		<script>
+			$(document).on('change', '#name', function(){
+				var name = $('#name :selected').val();//注意:selected前面有個空格！
+				$.ajax({
+					url:"process.php",				
+					method:"POST",
+					data:{
+						name:name
+					},					
+					success:function(res){					
+					$('#color').html(res);
+					}
+				})//end ajax
+			});
+		</script>
+		<script>
+			$(document).on('change', '#color', function(){
+				var name = $('#name :selected').val();//注意:selected前面有個空格！
+				var color = $('#color :selected').val();//注意:selected前面有個空格！
+				$.ajax({
+					url:"process.php",				
+					method:"POST",
+					data:{
 						
-						<td> <select id="cmbMake1" name="color">
-						$document.on('change', '#name', function(){
-							name = $('#name :selected').val();
-							$ajax({
-							  url:"process.php",				
-							  method:"POST",
-							  data:{
-								 name:name
-							  },					
-							  success:function(res){					
-								   $('#color').html(res);
-							  }
-							})
-							});
-								
-						</select></td>						
-                        <td> <input type="text" name="id3" value=""></td>
-                        <td> <input type="text" name="id4" value=""></td>
-                        <td> <input type="text" name="id5" value=""></td>
-                        <td> <input type="text" name="id6" value=""></td>
-                        <td> <input type="text" name="id7" value=""></td>
-                    </tr>  
-                </table>
-            </div>
-            <?php
-            
-            function pre_r( $array ) {
-                echo '<pre>';
-                print_r($array);
-                echo '</pre>';
-            }
-        ?>
-        
-        <div class="row justify-content-center">
-        <form action="process.php" method="POST">
-            <input type="hidden" name="id" value="<?php echo $id; ?>">
-            <div class="form-group">
-            <label>Name</label>
-            <input type="text" name="name" class="form-control" 
-                   value="<?php echo $name; ?>" placeholder="Enter your name">
-            </div>
-            <div class="form-group">
-            <label>Brand</label>
-            <input type="text" name="brand" 
-                   value="<?php echo $brand; ?>" class="form-control" placeholder="Enter your brand">
-            </div>
-            <div class="form-group">
-            <?php 
-            if ($update == true): 
-            ?>
-                <button type="submit" class="btn btn-info" name="update">Update</button>
-            <?php else: ?>
-                <button type="submit" class="btn btn-primary" name="save">Save</button>
-            <?php endif; ?>
-            </div>
-        </form>
-        </div>
-        </div>
+						color:color,
+						xname:name
+					},					
+					success:function(res1){						
+					$('#material').html(res1);
+					$('#abc').html(value=res1);
+					}
+				})//end ajax
+			});
+		</script>
     </body>
 </html>
