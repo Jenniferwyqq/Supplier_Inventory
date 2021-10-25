07d2e634cec8fdfc09e2a3cdc55a548880d76b00
Jennifer Wu <71246844+Jenniferwyqq@users.noreply.github.com>
2021-10-25 00:28:15 -0700

    Update purchase.php

--- purchase.php
+++ purchase.php
@@ -20,40 +20,63 @@
         <div class="container">
         <?php
             $mysqli = new mysqli('localhost','root','123','top_shoe') or die(mysqli_error($mysqli));
-            $result = $mysqli->query("SELECT shoe.id AS shoeid, box.id AS boxid, purchase_id AS dateid, employ.id AS empid, shoe.name AS style, shoe.color AS color, box.name AS box, purchase.unit_price AS unit_qrice, purchase.quantity AS quantity, purchase_date.date AS date, employ.id AS employ_id FROM shoe, box, purchase_date, purchase, employ WHERE purchase.shoe_id = shoe.id AND purchase.box_id = box.id AND purchase.purchase_id = purchase_date.id AND purchase.employ_id = employ.id;") or die($mysqli->error);
-            //pre_r($result);
-			$result1 = $mysqli->query("SELECT DISTINCT shoe.name AS name FROM `shoe` WHERE 1;") or die($mysqli->error);
-			$result2 = $mysqli->query("SELECT DISTINCT shoe.color AS color FROM `shoe` WHERE 1;") or die($mysqli->error);
+			$resultbrand = $mysqli->query("SELECT DISTINCT brand.id AS brandid, brand.name AS brand FROM `brand` WHERE 1;") or die($mysqli->error);
+
+			$resultbox = $mysqli->query("SELECT DISTINCT box.name AS boxname FROM `box` WHERE 1") or die($mysqli->error);
+
         ?>
-			
+		</div>
+
+		<div class="row justify-content-center">
+			<?php 
+				date_default_timezone_set('UTC');
+				//$localtime = localtime();
+				//$m = date("m");
+				//$d = date("d");
+				//$y = date("y");
+				//$today = $y . '-' . $m . '-' . $d;
+				//$today = localtime();
+				//<input type="date" value="<?php echo date("Y-m-d");
+				
+			?>
+			<label for="date">Purchase Date</label>
+			<input type="date" id="date" name="date" value="<?php echo date("Y-m-d") ?>" min="2018-01-01" max="2030-12-31">
+
+		</div>
 		
-        
 		<div class="row justify-content-center">
 			<table class="table">
 				<thead>
 					<tr>
+						<th>BRAND</th>
 						<th>STYLE</th>
 						<th>COLOR</th>
 						<th>MATERIAL</th>
 						<th>BOX</th>
 						<th>QUANTITY</th>
-						<th colspan="2">Action</th>
+						<th>PRICE</th>
 					</tr>
 				</thead>
 				<div class="row justify-content-center">
 					<tr>
 						<td>
 							<div>
-								<select id='name'>
+								<select id='brand'>
 									<option>-----</option>
 									<?php
-									$result4 = $mysqli->query("SELECT DISTINCT shoe.name AS name FROM `shoe` WHERE 1;") or die($mysqli->error);
-									while($data=mysqli_fetch_assoc($result4)){
+									while($data=mysqli_fetch_assoc($resultbrand)){
 									?>
-										  <option value="<?php echo $data['name'];?>"><?php echo $data['name'];?></option>
+										  <option value="<?php echo $data['brandid'];?>"><?php echo $data['brand'];?></option>
 									<?php
 									}
 									?>
+								</select>
+							</div>
+						</td>
+						<td>
+							<div>
+								<select id='name'>
+									<option>-----</option>
 								</select>
 							</div>
 						</td>
@@ -75,71 +98,144 @@
 							<div>
 								<select id='box'>
 									<option>-----</option>
-									
-									<?php
-									$result5 = $mysqli->query("SELECT DISTINCT box.name FROM box, brand WHERE box.brand_id = 數字;") or die($mysqli->error);
-									while($data=mysqli_fetch_assoc($result5)){
-									?>
-										  <option value="<?php echo $data['name'];?>"><?php echo $data['name'];?></option>
-									<?php 
-									}
-									?>
 								</select>
 							</div>
 						</td>
 						<td>
-							<input type="text" name="name" value="" placeholder="Enter the quantity">
-						</td>
-						<td>
-							<button type="submit" class="btn btn-info" name="update">Update</button>   
+							<input type="text" placeholder="Enter quantity" id="quantity">
+							<input type="text" placeholder="Enter price" id="price">
+							<input type="button" value="ADD" onclick="getInputValue()"> 
 						</td>
 					</tr>
 				</div>
 			</table>
 		</div>
-		<input id = 'abc' type="text" style="font-size:12px" value="修改文字大小為 12px"><br>
-		<?php
 		
-		function pre_r( $array ) {
-			echo '<pre>';
-			print_r($array);
-			echo '</pre>';
-		}
-        ?>
-        
+		
+		
 		<script>
-			$(document).on('change', '#name', function(){
-				var name = $('#name :selected').val();//注意:selected前面有個空格！
+function msg() {
+  alert("Hello world!");
+}
+</script>
+        
+        <script>
+			$(document).on('change', '#brand', function(){
+				var brand = $('#brand :selected').val();//注意:selected前面有個空格！
 				$.ajax({
 					url:"process.php",				
 					method:"POST",
 					data:{
+						brand:brand
+					},					
+					success:function(res){		
+					res = "<option>-----</option>" +res;
+					$('#name').html(res);
+					}
+				})//end ajax
+			});
+		</script>
+		
+		<script>
+			$(document).on('change', '#name', function(){				
+				var brand = $('#brand :selected').val();
+				var name = $('#name :selected').val();
+				$.ajax({
+					url:"process.php",				
+					method:"POST",
+					data:{
+						brand1:brand,
 						name:name
 					},					
-					success:function(res){					
-					$('#color').html(res);
+					success:function(res1){		
+					res1 = "<option>-----</option>" +res1;					
+					$('#color').html(res1);
 					}
 				})//end ajax
 			});
 		</script>
 		<script>
 			$(document).on('change', '#color', function(){
-				var name = $('#name :selected').val();//注意:selected前面有個空格！
-				var color = $('#color :selected').val();//注意:selected前面有個空格！
+				var brand = $('#brand :selected').val();
+				var name = $('#name :selected').val();
+				var color = $('#color :selected').val();
 				$.ajax({
 					url:"process.php",				
 					method:"POST",
 					data:{
+						brand2:brand,
+						name1:name,
+						color:color
 						
-						color:color,
-						xname:name
 					},					
-					success:function(res1){						
-					$('#material').html(res1);
-					$('#abc').html(value=res1);
+					success:function(res2){		
+					res2 = "<option>-----</option>" +res2;
+					$('#material').html(res2);
 					}
 				})//end ajax
 			});
+		</script>
+		<script>
+			$(document).on('change', '#material', function(){
+				var brand = $('#brand :selected').val();
+				var name = $('#name :selected').val();
+				var color = $('#color :selected').val();
+				var material = $('#material :selected').val();
+				$.ajax({
+					url:"process.php",				
+					method:"POST",
+					data:{
+						brand3:brand,
+						name2:name,
+						color1:color,
+						material:material
+					},					
+					success:function(res3){		
+					res3 = "<option>-----</option>" +res3;					
+					$('#box').html(res3);
+					//$('#abc').html(value=res1);
+					}
+				})//end ajax
+			});
+		</script>
+		 <script>
+			
+			function getInputValue(){
+				
+				// Selecting the input element and get its value 
+				var brand = $('#brand :selected').val();
+				var name = $('#name :selected').val();
+				var color = $('#color :selected').val();
+				var material = $('#material :selected').val();
+				var quantity = document.getElementById("quantity").value;
+				var price = document.getElementById("price").value;
+				var purchasedate = $('#date').val();
+				alert(brand);
+				alert(purchasedate);
+				$.ajax({
+					url:"process.php",				
+					method:"POST",
+					data:{
+						brand4:brand,
+						name3:name,
+						color2:color,
+						material1:material,
+						quantity:quantity,
+						price:price,
+						purchasedate:purchasedate
+					},
+					success:function(res4){		
+						alert(res4);
+					},
+					error: function () {
+						alert("Local error callback.");
+					},
+					complete: function () {
+						alert("Local completion callback.");
+					}
+				})//end ajax
+				//alert("123");
+			}
 		</script>
     </body>
 </html>
