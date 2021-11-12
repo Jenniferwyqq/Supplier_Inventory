7d4486dc30cd0cdbf1bad2fc85e8b4faf45acc0c
Jennifer Wu <71246844+Jenniferwyqq@users.noreply.github.com>
2021-11-11 22:47:28 -0800

    Update boxprocess.php

--- boxprocess.php
+++ boxprocess.php
@@ -1,46 +1,19 @@
 <?php
 
 session_start();
-
 $mysqli = new mysqli('localhost', 'root', '123', 'top_shoe') or die(mysqli_error($mysqli));
 
-$id = 0;
-$update = false;
-$name = '';
-$brand = '';
-
-if (isset($_GET['search'])){
-    $id = $_GET['search'];
-	$brand = $_GET['brand'];
-	$name = $_GET['name'];
-	$color = $_GET['color'];
-	$update = true;
-    $result = $mysqli->query("SELECT box_id AS box_id, shoe_id AS shoe_id, length AS length, width AS width, height AS height, weight AS weight FROM box_dimension WHERE 1;") or die($mysqli->error());
-    while($data=mysqli_fetch_assoc($result)){
-        $row = $result->fetch_array();
-        $box_id = $row['box_id'];
-        $shoe_id = $row['shoe_id'];
-		$length = $row['length'];
-		$width = $row['width'];
-		$height = $row['height'];
-		$weight = $row['weight'];
-    };
-}
-
-
-
-	
 if (isset($_POST['brand'])){
 	$brand = $_POST['brand'];
 	$result1 = $mysqli->query("SELECT DISTINCT shoe.name AS name FROM shoe WHERE shoe.brand_id = '$brand';") or die($mysqli->error);
 
-	$res = "";//把準備回傳的變數res準備好
+	$res = "";
 	while($data=mysqli_fetch_assoc($result1)){
 	   $res .= "
 			<option value='{$data["name"]}'>{$data['name']}</option>
-	   ";//將對應的型號項目遞迴列出
+	   ";
 	};
-	echo $res;//將型號項目丟回給ajax
+	echo $res;
 }
 
 if (isset($_POST['name'])){
@@ -48,13 +21,13 @@ if (isset($_POST['name'])){
 	$name = $_POST['name'];
 	$result2 = $mysqli->query("SELECT DISTINCT shoe.color AS color FROM shoe WHERE shoe.name = '$name' AND shoe.brand_id = '$brand';") or die($mysqli->error);
 
-	$res1 = "";//把準備回傳的變數res準備好
+	$res1 = "";
 	while($data=mysqli_fetch_assoc($result2)){
 	   $res1 .= "
 		  <option value='{$data["color"]}'>{$data['color']}</option>
-	   ";//將對應的型號項目遞迴列出
+	   ";
 	};
-	echo $res1;//將型號項目丟回給ajax
+	echo $res1;
 }
 
 if (isset($_POST['color'])){
@@ -63,54 +36,52 @@ if (isset($_POST['color'])){
 	$color = $_POST['color'];
 	$result3 = $mysqli->query("SELECT DISTINCT shoe.material AS material FROM shoe WHERE shoe.brand_id = '$brand' AND shoe.name = '$name' AND shoe.color = '$color';") or die($mysqli->error);
 
-	$res2 = "";//把準備回傳的變數res準備好
+	$res2 = "";
 	while($data=mysqli_fetch_assoc($result3)){
 	   $res2 .= "
 		  <option value='{$data["material"]}'>{$data['material']}</option>
-	   ";//將對應的型號項目遞迴列出
+	   ";
 	};
-	echo $res2;//將型號項目丟回給ajax
-}
-
-if (isset($_POST['material'])){
-	$brand = $_POST['brand3'];
-	$name = $_POST['name2'];
-	$color = $_POST['color1'];
-	$material = $_POST['material'];
-	$result4 = $mysqli->query("SELECT DISTINCT box.id AS boxid, box.name AS box FROM box WHERE box.brand_id = '$brand';") or die($mysqli->error);
-
-	$res3 = "";//把準備回傳的變數res準備好
-	while($data=mysqli_fetch_assoc($result4)){
-	   $res3 .= "
-		  <option value='{$data["boxid"]}'>{$data['box']}</option>
-	   ";//將對應的型號項目遞迴列出
-	};
-	echo $res3;//將型號項目丟回給ajax
+	echo $res2;
 }
 
 if (isset($_POST['brand4'])){
-	$brand = $_POST['brand4'];
+    $brand = $_POST['brand4'];
 	$name = $_POST['name3'];
 	$color = $_POST['color2'];
 	$material = $_POST['material1'];
-	$box = $_POST['box'];
-	$res4 = "";
 	
-	//find shoe id
-	$result8 = $mysqli->query("SELECT DISTINCT shoe.id AS sid FROM shoe WHERE shoe.name = '$name' AND shoe.color = '$color' AND shoe.material = '$material' AND shoe.brand_id = '$brand';") or die($mysqli->error);
-	while($data2=mysqli_fetch_assoc($result8)){
+	//find shose_id
+	$result10 = $mysqli->query("SELECT DISTINCT shoe.id AS sid FROM shoe WHERE shoe.name = '$name' AND shoe.color = '$color' AND shoe.material = '$material' AND shoe.brand_id = '$brand';") or die($mysqli->error);
+	while($data2=mysqli_fetch_assoc($result10)){
         $shoe_id=$data2['sid'];
     }
-	
-	$resultbn = $mysqli->query("SELECT length AS length, width AS width, height AS height, weight AS weight FROM box_dimension WHERE box_dimension.shoe_id = '$shoe_id' AND box_dimension.box_id = $box;") or die($mysqli->error);
-	
-	while($test=mysqli_fetch_assoc($resultbn)){ 
-			$res4.="".$test['length'].",".$test['width'].",".$test['height'].",".$test['weight']."";
 
-    }
-	echo $res4;
+	//$sql = "SELECT box_dimension.length AS length, box_dimension.width AS width, box_dimension.height AS height, box_dimension.weight AS weight FROM box_dimension WHERE box_dimension.shoe_id = $shoe_id;";
+	$sql = $mysqli->query("SELECT * FROM box_dimension WHERE box_dimension.shoe_id = $shoe_id;") or die($mysqli->error);
 
+	//$sql = $mysqli->query("SELECT * FROM box_dimension WHERE 1;") or die($mysqli->error());
+	
+	$divStr = "<table class=\"table\"><tr><th>box</th><th>length</th><th>width</th><th>height</th><th>weight</th></tr>";
+
+	while ($row = $sql->fetch_assoc()){ 
+		//find box_name
+
+
+		$box_id = $row['box_id'];
+		$result11 = $mysqli->query("SELECT DISTINCT box.name AS bname FROM box WHERE box.id = '$box_id';") or die($mysqli->error);
+		while($data3=mysqli_fetch_assoc($result11)){
+			$box_name=$data3['bname'];
+		}
+		
+		$divStr = $divStr . "<tr><td>" . $box_name . "</td><td>" . $row['length'] . "</td><td>" . $row['width'] . "</td><td>" . $row['height'] . "</td><td>" . $row['weight'] . "</td></tr>";
+	};   
+
+	$divStr = $divStr . "</table>";
+	
+	echo $divStr;
+
+	//header('location: 1.php');
 }
 
 ?>
-
