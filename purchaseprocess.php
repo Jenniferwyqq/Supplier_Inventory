6988148eb54adb641cac88f8c737b0e3cdfc7fc9
Jennifer Wu <71246844+Jenniferwyqq@users.noreply.github.com>
2021-11-12 11:45:07 -0800

    Update purchaseprocess.php

--- purchaseprocess.php
+++ purchaseprocess.php
@@ -115,7 +115,7 @@ if (isset($_POST['material'])){
 	};
 	echo $res3;//將型號項目丟回給ajax
 }
-
+/*
 if (isset($_POST['brand4'])){
 	$brand = $_POST['brand4'];
 	$name = $_POST['name3'];
@@ -126,9 +126,8 @@ if (isset($_POST['brand4'])){
 	$price = $_POST['price'];
 	$purchasedate = $_POST['purchasedate'];
 	$res4 = "test";
+
 	$result5 = $mysqli->query("SELECT DISTINCT purchase_date.id AS pid FROM purchase_date WHERE purchase_date.date = '$purchasedate' AND purchase_date.brand_id = '$brand';") or die($mysqli->error);
-	
-	
 	while($data=mysqli_fetch_assoc($result5)){
         $res4=$data['pid'];
     }
@@ -146,7 +145,73 @@ if (isset($_POST['brand4'])){
 			echo  "Error: " . $result6;
 		};
     };
-	echo $res4;
+	
+	$resultbrand = $mysqli->query("SELECT * FROM brand WHERE brand.id = '$brand';") or die($mysqli->error);
+	while($brand=mysqli_fetch_assoc($resultbrand)){
+        $brname=$brand['name'];
+    }
+	
+	//find shoe id
+	$result8 = $mysqli->query("SELECT DISTINCT shoe.id AS sid FROM shoe WHERE shoe.name = '$name' AND shoe.color = '$color' AND shoe.material = '$material' AND shoe.brand_id = '$brand';") or die($mysqli->error);
+	while($data2=mysqli_fetch_assoc($result8)){
+        $shoe_id=$data2['sid'];
+    }
+	
+	//insert data to purchase, employee set 1
+	$result9 = $mysqli->query("INSERT INTO purchase (purchase_id, box_id, shoe_id, quantity, unit_price, employ_id) VALUES ('$res4', '$box', '$shoe_id', '$quantity', '$price', '1');") or die($mysqli->error);
+	
+	$divStr = "<table class=\"table\"><tr><th>brand</th><th>style</th><th>color</th><th>material</th><th>box</th><th>quantity</th><th>price</th></tr>";
+	
+	$divStr = $divStr . "<tr><td>" . $brname . "</td><td>" .  $name . "</td><td>" . $color . "</td><td>" . $material . "</td><td>" . $box . "</td><td>" . $quantity . "</td><td>" . $price . "</td></tr>";
+
+	$divStr = $divStr . "</table>";
+	echo $divStr;
+}*/
+
+if (isset($_POST['brand4'])){
+	$brand = $_POST['brand4'];
+	$name = $_POST['name3'];
+	$color = $_POST['color2'];
+	$material = $_POST['material1'];
+	$box = $_POST['box'];
+	$quantity = $_POST['quantity'];
+	$price = $_POST['price'];
+	$purchasedate = $_POST['purchasedate'];
+	$res4 = "test";
+	
+	//brand name
+	$brandname = $mysqli->query("SELECT brand.name AS brn FROM brand WHERE brand.id = '$brand';") or die($mysqli->error);
+	while($databrand=mysqli_fetch_assoc($brandname)){
+        $brname=$databrand['brn'];
+    }
+	
+	//brand name
+	$boxname = $mysqli->query("SELECT box.name AS bn FROM box WHERE box.id = '$box';") or die($mysqli->error);
+	while($databox=mysqli_fetch_assoc($boxname)){
+        $bname=$databox['bn'];
+    }
+	
+	//find if data in purchase_date
+	$result5 = $mysqli->query("SELECT DISTINCT purchase_date.id AS pid FROM purchase_date WHERE purchase_date.date = '$purchasedate' AND purchase_date.brand_id = '$brand';") or die($mysqli->error);
+	$divStr = "<table class=\"table\"><tr><th>brand</th><th>style</th><th>color</th><th>material</th><th>box</th><th>quantity</th><th>price</th><th>status</th></tr>";
+
+	while($data=mysqli_fetch_assoc($result5)){
+        $res4=$data['pid'];
+    }
+	
+    if($res4=="test"){
+		$result6 = $mysqli->query("INSERT INTO purchase_date (date, brand_id) VALUES ('$purchasedate', '$brand');") or die($mysqli->error);
+		if($result6==true){
+			$result7 = $mysqli->query("SELECT DISTINCT purchase_date.id AS pid FROM purchase_date WHERE purchase_date.date = '$purchasedate' AND purchase_date.brand_id = '$brand';") or die($mysqli->error);
+			while($data1=mysqli_fetch_assoc($result7)){
+				$res4=$data1['pid'];
+			}
+		}		
+		else{
+			echo  "Error: " . $result6;
+		};
+    };
+	
 	
 	//find shoe id
 	$result8 = $mysqli->query("SELECT DISTINCT shoe.id AS sid FROM shoe WHERE shoe.name = '$name' AND shoe.color = '$color' AND shoe.material = '$material' AND shoe.brand_id = '$brand';") or die($mysqli->error);
@@ -157,7 +222,11 @@ if (isset($_POST['brand4'])){
 	
 	//insert data to purchase, employee set 1
 	$result9 = $mysqli->query("INSERT INTO purchase (purchase_id, box_id, shoe_id, quantity, unit_price, employ_id) VALUES ('$res4', '$box', '$shoe_id', '$quantity', '$price', '1');") or die($mysqli->error);
+	
+	$divStr = $divStr . "<tr><td>" . $brname . "</td><td>" .  $name . "</td><td>" . $color . "</td><td>" . $material . "</td><td>" . $bname . "</td><td>" . $quantity . "</td><td>" . $price . "</td><td>" . "updated success!" . "</td></tr>";
 
+	$divStr = $divStr . "</table>";
+	echo $divStr;
 }
 ?>
 
