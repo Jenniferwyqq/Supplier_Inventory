c4dc74600febe6eef93adff3019adf3faacb5ec1
Jennifer Wu <71246844+Jenniferwyqq@users.noreply.github.com>
2021-11-24 00:43:19 -0800

    Update shoeprocess.php

--- shoeprocess.php
+++ shoeprocess.php
@@ -1,100 +1,152 @@
 <?php
 
 session_start();
-
 $mysqli = new mysqli('localhost', 'root', '123', 'top_shoe') or die(mysqli_error($mysqli));
-
-$id = 0;
-$update = false;
-$name = '';
-$brand = '';
-
-if (isset($_GET['search'])){
-    $id = $_GET['search'];
-	$brand = $_GET['brand'];
-	$name = $_GET['name'];
-	$color = $_GET['color'];
-	$update = true;
-    $result = $mysqli->query("SELECT box_id AS box_id, shoe_id AS shoe_id, length AS length, width AS width, height AS height, weight AS weight FROM box_dimension WHERE 1;") or die($mysqli->error());
-    while($data=mysqli_fetch_assoc($result)){
-        $row = $result->fetch_array();
-        $box_id = $row['box_id'];
-        $shoe_id = $row['shoe_id'];
-		$length = $row['length'];
-		$width = $row['width'];
-		$height = $row['height'];
-		$weight = $row['weight'];
-    };
-}
-
 
 
 	
 if (isset($_POST['brand'])){
 	$brand = $_POST['brand'];
-	$result1 = $mysqli->query("SELECT DISTINCT shoe.name AS name FROM shoe WHERE shoe.brand_id = '$brand';") or die($mysqli->error);
+	$shoe_name = $mysqli->query("SELECT DISTINCT shoe.name AS name FROM shoe WHERE shoe.brand_id = '$brand';") or die($mysqli->error);
 
-	$res = "";//把準備回傳的變數res準備好
-	while($data=mysqli_fetch_assoc($result1)){
-	   $res .= "
+	$shoename = "";
+	while($data=mysqli_fetch_assoc($shoe_name)){
+	   $shoename .= "
 			<option value='{$data["name"]}'>{$data['name']}</option>
-	   ";//將對應的型號項目遞迴列出
+	   ";
 	};
-	echo $res;//將型號項目丟回給ajax
+	echo $shoename;
 }
 
 if (isset($_POST['name'])){
 	$brand = $_POST['brand1'];
 	$name = $_POST['name'];
-	$result2 = $mysqli->query("SELECT DISTINCT shoe.color AS color FROM shoe WHERE shoe.name = '$name' AND shoe.brand_id = '$brand';") or die($mysqli->error);
+	$shoe_color = $mysqli->query("SELECT DISTINCT shoe.color AS color FROM shoe WHERE shoe.name = '$name' AND shoe.brand_id = '$brand';") or die($mysqli->error);
 
-	$res1 = "";//把準備回傳的變數res準備好
-	while($data=mysqli_fetch_assoc($result2)){
-	   $res1 .= "
-		  <option value='{$data["color"]}'>{$data['color']}</option>
-	   ";//將對應的型號項目遞迴列出
+	$shoecolor = "";
+	while($data1=mysqli_fetch_assoc($shoe_color)){
+	   $shoecolor .= "
+		  <option value='{$data1["color"]}'>{$data1['color']}</option>
+	   ";
 	};
-	echo $res1;//將型號項目丟回給ajax
+	echo $shoecolor;
 }
 
 if (isset($_POST['color'])){
 	$brand = $_POST['brand2'];
 	$name = $_POST['name1'];
 	$color = $_POST['color'];
-	$result3 = $mysqli->query("SELECT DISTINCT shoe.material AS material FROM shoe WHERE shoe.brand_id = '$brand' AND shoe.name = '$name' AND shoe.color = '$color';") or die($mysqli->error);
+	$shoe_material = $mysqli->query("SELECT DISTINCT shoe.material AS material FROM shoe WHERE shoe.brand_id = '$brand' AND shoe.name = '$name' AND shoe.color = '$color';") or die($mysqli->error);
 
-	$res2 = "";//把準備回傳的變數res準備好
-	while($data=mysqli_fetch_assoc($result3)){
-	   $res2 .= "
-		  <option value='{$data["material"]}'>{$data['material']}</option>
-	   ";//將對應的型號項目遞迴列出
+	$shoematerial = "";
+	while($data2=mysqli_fetch_assoc($shoe_material)){
+	   $shoematerial .= "
+		  <option value='{$data2["material"]}'>{$data2['material']}</option>
+	   ";
 	};
-	echo $res2;//將型號項目丟回給ajax
+	echo $shoematerial;
 }
 
-if (isset($_POST['brand4'])){
+//display info
+if (isset($_POST['material'])){
+	$brand = $_POST['brand3'];
+	$name = $_POST['name2'];
+	$color = $_POST['color1'];
+	$material = $_POST['material'];
+	$shoe_id = 0;
+	$search_shoe1 = $mysqli->query("SELECT DISTINCT shoe.id AS sid FROM shoe WHERE shoe.name = '$name' AND shoe.brand_id = '$brand' AND shoe.color = '$color' AND shoe.material = '$material';") or die($mysqli->error);
+	while($data2=mysqli_fetch_assoc($search_shoe1)){
+		$shoe_id = $data2['sid'];
+	}	
+	
+	//shoe_dimensino_table
+	$divStr = "<table id=\"shoetable\" class=\"table table-hover table-bordered table-condensed table-striped\"><tr><th>size</th><th>length</th><th>width</th><th>height</th><th>weight</th><th></th></tr>";
+	$dimension = $mysqli->query("SELECT * FROM shoe_dimension WHERE shoe_dimension.shoe_id = '$shoe_id';") or die($mysqli->error);
+	$dimension->fetch_assoc();
+	
+	while ($row = $dimension->fetch_assoc()){
+		$size = $row['size'];
+		$length = $row['length'];
+		$width = $row['width'];
+		$height = $row['height'];
+		$weight = $row['weight'];
+		$editbtn = "<button class=\"saveChanges\" id=\"$size\" onclick=\"editclick(this);\">UPDATE</button>";
+		$divStr = $divStr . "<tr id\"$size\"><td>" . $size . "</td><td contenteditable=\"true\">" .  $length . "</td><td contenteditable=\"true\">" .  $width . "</td><td contenteditable=\"true\">" .  $height . "</td><td contenteditable=\"true\">" .  $weight . "</td><td>" .  $editbtn . "</td></tr>";
+	}
+	$divStr = $divStr . "</table>";
+	
+	//box_dimensino_table
+	$divStr1 = "<table id=\"boxtable\" class=\"table table-hover table-bordered table-condensed table-striped\"><tr><th>box</th><th>length</th><th>width</th><th>height</th><th>weight</th><th></th></tr>";
+	$dimension1 = $mysqli->query("SELECT * FROM box_dimension WHERE box_dimension.shoe_id = '$shoe_id';") or die($mysqli->error);
+	
+	while ($row1 = $dimension1->fetch_assoc()){
+		$box = $row1['box_id'];
+		$length = $row1['length'];
+		$width = $row1['width'];
+		$height = $row1['height'];
+		$weight = $row1['weight'];
+		$editbtn = "<button class=\"hasChanges\" id=\"$box\" onclick=\"editclick(this.id)\">UPDATE</button>";
+		
+		$boxname = $mysqli->query("SELECT DISTINCT box.name AS bname FROM box WHERE box.id = '$box';") or die($mysqli->error);
+		while ($row2 = $boxname->fetch_assoc()){
+			$box_name = $row2['bname'];
+		}
+		$divStr1 = $divStr1 . "<tr><td>" . $box_name . "</td><td contenteditable=\"true\">" .  $length . "</td><td contenteditable=\"true\">" .  $width . "</td><td contenteditable=\"true\">" .  $height . "</td><td contenteditable=\"true\">" .  $weight . "</td><td>" .  $editbtn . "</td></tr>";
+	}
+	$divStr1 = $divStr1 . "</table>";
+	
+	$divStr2 = "
+	<div>
+		<h3> Select File To Upload </h3>
+		<form method='post' action='' enctype='multipart/form-data'>
+			<input type='file' name='image' ><br>
+			<input type=\"hidden\" id=\"shoe_id\" name=\"shoe_id\" value=\"". $shoe_id ."\">
+			<input type='submit' value='Upload Image' name='upload'>  
+		</form>
+	<div class=\"row\">
+	<table border=\"2\">
+		<tr>
+			<td>No.</td>
+			<td>Images</td>
+		</tr>";
+	$no = 1;
+	$records = mysqli_query($mysqli,"select * from photo WHERE photo.shoe_id = $shoe_id "); 
+	while($data3 = mysqli_fetch_array($records)){
+		$path = "image/" . $data3['image'];
+		
+		$photo = "<img style=\"max-height: 400px ;max-width: 500px\" src= '" . $path . "' >";
+		$divStr2 = $divStr2 . "<tr><td>" . $no . "</td><td>" . $photo . "</td></tr>";
+		$no++;
+	}
+	$divStr2 = $divStr2 . "</table></div>";
+	
+
+	echo json_encode(array($divStr, $divStr1, $divStr2, $shoe_id));
+}
+
+//update shoe_dimension
+if (isset($_POST['size'])){
 	$brand = $_POST['brand4'];
 	$name = $_POST['name3'];
 	$color = $_POST['color2'];
 	$material = $_POST['material1'];
-	
+	$size = $_POST['size'];
+	$length = $_POST['length'];
+	$width = $_POST['width'];
+	$height = $_POST['height'];
+	$weight = $_POST['weight'];
+	$result = "FAIL!";
 	//find shoe id
-	$result10 = $mysqli->query("SELECT DISTINCT shoe.id AS sid FROM shoe WHERE shoe.name = '$name' AND shoe.color = '$color' AND shoe.material = '$material' AND shoe.brand_id = '$brand';") or die($mysqli->error);
-	while($data2=mysqli_fetch_assoc($result10)){
-        $shoe_id=$data2['sid'];
+	$search_shoe2 = $mysqli->query("SELECT DISTINCT shoe.id AS sid FROM shoe WHERE shoe.name = '$name' AND shoe.color = '$color' AND shoe.material = '$material' AND shoe.brand_id = '$brand';") or die($mysqli->error);
+	while($data4=mysqli_fetch_assoc($search_shoe2)){
+        $shoe_id1=$data4['sid'];
     }
 	
-	$sql = $mysqli->query("SELECT * FROM shoe_dimension WHERE shoe_dimension.shoe_id = '$shoe_id';") or die($mysqli->error);
-	
-	$divStr = "<table class=\"table\"><tr><th>size</th><th>length</th><th>width</th><th>height</th><th>weight</th></tr>";
-	
-	
-	while ($row = $sql->fetch_assoc()){ 
-		
-		$divStr = $divStr . "<tr><td>" . $row['size'] . "</td><td>" . $row['length'] . "</td><td>" . $row['width'] . "</td><td>" . $row['height'] . "</td><td>" . $row['weight'] . "</td></tr>";
-	};   
-	$divStr = $divStr . "</table>";
-	echo $divStr;
+	$update_dimension = $mysqli->query("UPDATE shoe_dimension SET shoe_dimension.length = '$length', shoe_dimension.width = '$width', shoe_dimension.height = '$height', shoe_dimension.weight = '$weight' WHERE shoe_dimension.shoe_id = '$shoe_id1' AND shoe_dimension.size = '$size';") or die($mysqli->error);
+	if($update_dimension==true){
+		$result = "UPDATE SUCCESS!";
+	}
+	echo $result;
 }
 
 
@@ -102,4 +154,3 @@ if (isset($_POST['brand4'])){
 
 
 ?>
-
