ffa3aca2e74d9cc676de2aa2bf85e8bf735e75c8
Jennifer Wu <71246844+Jenniferwyqq@users.noreply.github.com>
2021-11-24 16:58:46 -0800

    Update inventory.php

--- inventory.php
+++ inventory.php
@@ -13,82 +13,104 @@
     </head>
     <body>
         <?php require_once 'inventoryprocess.php'; ?>
-        
-        <div class="container">
-			<?php
-				$mysqli = new mysqli('localhost','root','123','top_shoe') or die(mysqli_error($mysqli));
-				$resultbrand = $mysqli->query("SELECT DISTINCT brand.id AS brandid, brand.name AS brand FROM `brand` WHERE 1;") or die($mysqli->error);
-				
-				
-			?>
-		</div>
+		<?php
+			$mysqli = new mysqli('localhost','root','123','top_shoe') or die(mysqli_error($mysqli));
+			$resultbrand = $mysqli->query("SELECT DISTINCT brand.id AS brandid, brand.name AS brand FROM `brand` WHERE 1;") or die($mysqli->error);
+		?>
 		<div class="container">
-			<table id="main" class="table table-bordered">
-				<thead>
+			<div class="row row justify-content-center">
+				<h3> Inventory </h3>
+			</div>
+			<div class="row">
+				<table id="main" class="table table-bordered">
+					<thead>
+						<tr>
+							<th>BRAND</th>
+							<th>STYLE</th>
+							<th>COLOR</th>
+							<th>MATERIAL</th>
+							<th></th>
+						</tr>
+					</thead>
 					<tr>
-						<th>BRAND</th>
-						<th>STYLE</th>
-						<th>COLOR</th>
-						<th>MATERIAL</th>
-						<th></th>
+						<td>
+							<div>
+								<select id='brand'>
+									<option>-----</option>
+									<?php
+									while($data=mysqli_fetch_assoc($resultbrand)){
+									?>
+										  <option value="<?php echo $data['brandid'];?>"><?php echo $data['brand'];?></option>
+									<?php
+									}
+									?>
+								</select>
+							</div>
+						</td>
+						<td>
+							<div>
+								<select id='name'>
+									<option>-----</option>
+								</select>
+							</div>
+						</td>
+						<td>
+							<div>
+								<select id='color'>
+									<option>-----</option>
+								</select>
+							</div>
+						</td>
+						<td>
+							<div>
+								<select id='material'>
+									<option>-----</option>
+								</select>
+							</div>
+						</td>
+						<td>
+							<input type="hidden" id="shoeID" name="shoeID" value="0">
+							<input type="button" class="btn btn-secondary" value="SEARCH" onclick="setdivcontent()"> 
+						</td>
 					</tr>
-				</thead>
-				<tr>
-					<td>
-						<div>
-							<select id='brand'>
-								<option>-----</option>
-								<?php
-								while($data=mysqli_fetch_assoc($resultbrand)){
-								?>
-									  <option value="<?php echo $data['brandid'];?>"><?php echo $data['brand'];?></option>
-								<?php
-								}
-								?>
-							</select>
-						</div>
-					</td>
-					<td>
-						<div>
-							<select id='name'>
-								<option>-----</option>
-							</select>
-						</div>
-					</td>
-					<td>
-						<div>
-							<select id='color'>
-								<option>-----</option>
-							</select>
-						</div>
-					</td>
-					<td>
-						<div>
-							<select id='material'>
-								<option>-----</option>
-							</select>
-						</div>
-					</td>
-					<td>
-						<input type="button" class="btn btn-secondary" value="SEARCH" onclick="setdivcontent()"> 
-					</td>
-				</tr>
-			</table>
+				</table>
+			</div>
+			
+			<div class="row">
+				<div class="col-5">
+					<div id="here"></div>
+				</div>
+				<div class="col-7">
+					<div class="row">
+						<div id ="view"></div>
+					</div>
+					<div class="row">
+						<div id="display"></div>
+					</div>
+				</div>
+			</div>
 		</div>
+		
 		<style>
 			th {background:#b8def5}
-		</style>
-		<style>
+			
 			.table-hover tbody tr:hover td {
+				background: #C8BFE7;
+			}
+		
+			.table-hover1 tbody tr:hover td {
 				background: #FEF878;
 			}
+			
+			.table{
+				table-layout:fixed;
+			}
+						
+			/* Add a background color on hover */
+			.view:hover {
+			  background-color: #3e8e41;
+			}
 		</style>
-		<div class="container" id="here">
-	
-		</div>
-		<div class="container" id="view">
-		</div>
-
 
         <script>
 			$(document).on('change', '#brand', function(){
@@ -99,9 +121,9 @@
 					data:{
 						brand:brand
 					},					
-					success:function(s_name){		
-					s_name = "<option>-----</option>" +s_name;
-					$('#name').html(s_name);
+					success:function(name){		
+					name = "<option>-----</option>" + name;
+					$('#name').html(name);
 					}
 				})
 			});
@@ -117,9 +139,9 @@
 						brand1:brand,
 						name:name
 					},					
-					success:function(s_color){		
-					s_color = "<option>-----</option>" +s_color;					
-					$('#color').html(s_color);
+					success:function(color){		
+					color = "<option>-----</option>" +color;					
+					$('#color').html(color);
 					}
 				})
 			});
@@ -137,21 +159,19 @@
 						name1:name,
 						color:color
 					},					
-					success:function(s_material){		
-					s_material = "<option>-----</option>" +s_material;
-					$('#material').html(s_material);
+					success:function(material){		
+					material = "<option>-----</option>" + material;
+					$('#material').html(material);
 					}
 				})
 			});
 		</script>
 		 <script>
 			function setdivcontent(){
-				
 				var brand = $('#brand :selected').val();
 				var name = $('#name :selected').val();
 				var color = $('#color :selected').val();
 				var material = $('#material :selected').val();
-				
 				$.ajax({
 					url:"inventoryprocess.php",				
 					method:"POST",
@@ -161,17 +181,15 @@
 						color1:color,
 						material:material
 					},
-					success:function(divStr){	
-						document.getElementById("here").innerHTML = divStr;
+					success:function(array){	
+						var result = $.parseJSON(array);
+						document.getElementById("shoeID").value = result[1];
+						document.getElementById("here").innerHTML = result[0];
 					},
 					error: function (divStr) {
 						alert("Local error callback.");
 					},
-					complete: function (divStr) {
-						//alert("Local completion callback.");
-					}
-				})//end ajax
-				//window.location.reload();
+				})
 			}
 		</script>
 		<script>
@@ -179,33 +197,25 @@
 				var yes = confirm('Are you sure？');
 				if (yes) {
 					var currentRow=$(this).closest("tr"); 
-					var box=currentRow.find("td:eq(0)").text();
-					var content=currentRow.find("td:eq(1)").text(); // get current row 1st TD value
-					var brand = $('#brand :selected').val();
-					var name = $('#name :selected').val();
-					var color = $('#color :selected').val();
-					var material = $('#material :selected').val();
-					
+					var box_name=currentRow.find("td:eq(0)").text();
+					var content=currentRow.find("td:eq(1)").text(); 
+					var shoe_id = document.getElementById("shoeID").value;
+					var brand_id = $('#brand :selected').val();
 					$.ajax({
 						url:"inventoryprocess.php",
 						type:"POST",
 						data:{
-							brand4:brand,
-							name3:name,
-							color2:color,
-							material1:material,
-							box:box,
+							shoe_id:shoe_id,
+							brand4:brand_id,
+							box:box_name,
 							content:content
 						},
 						success:function(divStr1){	
-							alert("update success");
+							alert(divStr1);
 						},
 						error: function (divStr1) {
 							alert("Local error callback.");
 						},
-						complete: function (divStr1) {
-							//alert("Local completion callback.");
-						}
 					});
 				}
 			});
@@ -213,32 +223,37 @@
 
 		<script>
 			function detailclick(clicked_id){
+				var shoe_id = document.getElementById("shoeID").value;
 				var brand = $('#brand :selected').val();
-				var name = $('#name :selected').val();
-				var color = $('#color :selected').val();
-				var material = $('#material :selected').val();
-
 				$.ajax({
 					url:"inventoryprocess.php",				
 					method:"POST",
 					data:{
+						shoe_id1:shoe_id,
 						brand5:brand,
-						name4:name,
-						color3:color,
-						material2:material,
 						box1:clicked_id
 					},
-					success:function(divStr1){		
-						document.getElementById("view").innerHTML = divStr1;
+					success:function(array){		
+						var result = $.parseJSON(array);
+						document.getElementById("view").innerHTML = result[3];
+						document.getElementById("display").innerHTML = result[0];
+						const shoebtn = document.getElementById("purchase");
+						const boxbtn = document.getElementById("sell");
+						const photobtn = document.getElementById("revise");
+						shoebtn.onclick = function () {
+						 	document.getElementById("display").innerHTML = result[0];
+						};
+						boxbtn.onclick = function () {
+							document.getElementById("display").innerHTML = result[1];
+						};
+						photobtn.onclick = function () {
+							document.getElementById("display").innerHTML = result[2];
+						};
 					},
-					error: function (divStr1) {
+					error: function (array) {
 						alert("Local error callback.");
 					},
-					complete: function (divStr1) {
-						//alert("Local completion callback.");
-					}
-				})//end ajax
-				//window.location.reload();
+				})
 			}
 		</script>
     </body>
