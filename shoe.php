0eabdf79ee4b59ac71bbcedd466302971d6c866d
Jennifer Wu <71246844+Jenniferwyqq@users.noreply.github.com>
2021-11-25 18:43:33 -0800

    Update shoe.php

--- shoe.php
+++ shoe.php
@@ -12,74 +12,85 @@
 			$resultbrand = $mysqli->query("SELECT DISTINCT brand.id AS brandid, brand.name AS brand FROM `brand` WHERE 1;") or die($mysqli->error);
         ?>
 		<div class="container">
-			<div style="text-align:center;">
-				<h2> Shoe Catalog </h2>
+			<div class="row row justify-content-center">
+				<h2> Product Catalog </h2>
 			</div>
-			<table id="main" class="table table-bordered">
-				<thead>
+			<div class="row">
+				<table id="main" class="table table-bordered">
+					<thead>
+						<tr>
+							<th>BRAND</th>
+							<th>STYLE</th>
+							<th>COLOR</th>
+							<th>MATERIAL</th>
+							<th></th>
+						</tr>
+					</thead>
 					<tr>
-						<th>BRAND</th>
-						<th>STYLE</th>
-						<th>COLOR</th>
-						<th>MATERIAL</th>
-						<th></th>
+						<td>
+							<div>
+								<select id='brand'>
+									<option>-----</option>
+									<?php
+									while($data=mysqli_fetch_assoc($resultbrand)){
+									?>
+										 <option value="<?php echo $data['brandid'];?>"><?php echo $data['brand'];?></option>
+									<?php
+									}
+									?>
+								</select>
+							</div>
+						</td>
+						<td>
+							<div>
+								<select id='name'>
+									<option>-----</option>
+								</select>
+							</div>
+						</td>
+						<td>
+							<div>
+								<select id='color'>
+									<option>-----</option>
+								</select>
+							</div>
+						</td>
+						<td>
+							<div>
+								<select id='material'>
+									<option>-----</option>
+								</select>
+							</div>
+						</td>
+						<td>
+							<input type="hidden" id="shoe_id" name="shoe_id" value="0">
+							<input type="button" value="SEARCH" onclick="setdivcontent()"> 
+						</td>
 					</tr>
-				</thead>
-				<tr>
-					<td>
-						<div>
-							<select id='brand'>
-								<option>-----</option>
-								<?php
-								while($data=mysqli_fetch_assoc($resultbrand)){
-								?>
-									 <option value="<?php echo $data['brandid'];?>"><?php echo $data['brand'];?></option>
-								<?php
-								}
-								?>
-							</select>
-						</div>
-					</td>
-					<td>
-						<div>
-							<select id='name'>
-								<option>-----</option>
-							</select>
-						</div>
-					</td>
-					<td>
-						<div>
-							<select id='color'>
-								<option>-----</option>
-							</select>
-						</div>
-					</td>
-					<td>
-						<div>
-							<select id='material'>
-								<option>-----</option>
-							</select>
-						</div>
-					</td>
-					<td>
-						<input type="hidden" id="shoe_id" name="shoe_id" value="0">
-						<input type="button" value="SEARCH" onclick="setdivcontent()"> 
-					</td>
-				</tr>
-			</table>
-		
+				</table>
+			</div>
+			<div class="row" id="displaybtn">
+			</div>
+			<div class="row" id="display">
+			</div>
+			
 			<style>
 				th {background:#b8def5;}
 				.table-hover tbody tr:hover td {
 					background: #FEF878;
 				}
 				.btn-group button {
-				  background-color: #04AA6D; /* Green background */
-				  border: 1px solid green; /* Green border */
+				  background-color: #7097c1; 
 				  color: white; /* White text */
-				  padding: 10px 24px; /* Some padding */
+				  font-size: 16px;
+				  border-radius: 8px;
+				  padding: 8px 30px; /* Some padding */
 				  cursor: pointer; /* Pointer/hand icon */
 				  float: left; /* Float the buttons side by side */
+				}
+
+				.table{
+					table-layout:fixed;
 				}
 
 				/* Clear floats (clearfix hack) */
@@ -95,16 +106,11 @@
 
 				/* Add a background color on hover */
 				.btn-group button:hover {
-				  background-color: #3e8e41;
+				  background-color: #36577c;
 				}
+				
+				iframe.hidden{ display:none }
 			</style>
-			<div class="btn-group">
-			    <button id="shoe_dim_btn">shoe_dimension</button>
-			    <button id="box_dim_btn">box_dimension</button>
-				<button id="photo_btn">photo</button>
-			</div>
-			<div id="display">
-			</div>
 		</div>
 	
         <script>
@@ -116,14 +122,13 @@
 					data:{
 						brand:brand
 					},					
-					success:function(shoename){		
-					shoename = "<option>-----</option>" +shoename;
-					$('#name').html(shoename);
+					success:function(name){		
+					name = "<option>-----</option>" + name;
+					$('#name').html(name);
 					}
 				})
 			});
 		</script>
-		
 		<script>
 			$(document).on('change', '#name', function(){				
 				var brand = $('#brand :selected').val();
@@ -135,9 +140,9 @@
 						brand1:brand,
 						name:name
 					},					
-					success:function(shoecolor){		
-					shoecolor = "<option>-----</option>" +shoecolor;					
-					$('#color').html(shoecolor);
+					success:function(color){		
+					color = "<option>-----</option>" + color;					
+					$('#color').html(color);
 					}
 				})
 			});
@@ -155,9 +160,9 @@
 						name1:name,
 						color:color
 					},					
-					success:function(shoematerial){		
-					shoematerial = "<option>-----</option>" +shoematerial;
-					$('#material').html(shoematerial);
+					success:function(material){		
+					material = "<option>-----</option>" + material;
+					$('#material').html(material);
 					}
 				})
 			});
@@ -179,19 +184,18 @@
 						material:material
 					},
 					success:function(array){
-						alert("select shoe_dimension / box_dimension / photo");
 						var result = $.parseJSON(array);
+						alert(result[4]);
+						document.getElementById("displaybtn").innerHTML = result[4];
 						document.getElementById("shoe_id").value = result[3];
 						const shoebtn = document.getElementById("shoe_dim_btn");
 						const boxbtn = document.getElementById("box_dim_btn");
 						const photobtn = document.getElementById("photo_btn");
-						alert(array);
+						document.getElementById("display").innerHTML = result[0];
 						shoebtn.onclick = function () {
-							//alert("HI");
 						 	document.getElementById("display").innerHTML = result[0];
 						};
 						boxbtn.onclick = function () {
-							//alert("deeee");
 							document.getElementById("display").innerHTML = result[1];
 						};
 						photobtn.onclick = function () {
@@ -200,39 +204,27 @@
 					},
 					error: function (array) {
 						alert("Local error callback.");
-					},
-					complete: function (array) {
-						//alert("Local completion callback.");
 					}
 				})
 			}
 		</script>
 		<script>
+			//update shoe dimension
 			$("#display").on('click','.saveChanges',function(){
-			//function editclick(ixd){
-				//alert(ixd);
 				var yes = confirm('Are you sure？');
 				if (yes) {
-					var brand = $('#brand :selected').val();
-					var name = $('#name :selected').val();
-					var color = $('#color :selected').val();
-					var material = $('#material :selected').val();
+					var shoe_id = document.getElementById("shoe_id").value;
 					var $currentRow=$(this).closest("tr"); 
 					var size=$currentRow.find("td:eq(0)").text();
 					var length=$currentRow.find("td:eq(1)").text();
 					var width=$currentRow.find("td:eq(2)").text();
 					var height=$currentRow.find("td:eq(3)").text();
 					var weight=$currentRow.find("td:eq(4)").text();
-					
-					
 					$.ajax({
 						url:"shoeprocess.php",
 						type:"POST",
 						data:{
-							brand4:brand,
-							name3:name,
-							color2:color,
-							material1:material,
+							shoe_id:shoe_id,
 							size:size,
 							length:length,
 							width:width,
@@ -242,11 +234,8 @@
 						success:function(divStr1){	
 							alert(divStr1);
 						},
-						error: function (divStr1) {
+						error: function () {
 							alert("Local error callback.");
-						},
-						complete: function (divStr1) {
-							//alert("Local completion callback.");
 						}
 					});
 				}
@@ -256,26 +245,21 @@
 			$("#display").on('click','.hasChanges',function(){
 				var yes = confirm('Are you sure？');
 				if (yes) {
+					var shoe_id = document.getElementById("shoe_id").value;
 					var brand = $('#brand :selected').val();
-					var name = $('#name :selected').val();
-					var color = $('#color :selected').val();
-					var material = $('#material :selected').val();
 					var $currentRow=$(this).closest("tr"); 
 					var box=$currentRow.find("td:eq(0)").text();
 					var length=$currentRow.find("td:eq(1)").text();
 					var width=$currentRow.find("td:eq(2)").text();
 					var height=$currentRow.find("td:eq(3)").text();
 					var weight=$currentRow.find("td:eq(4)").text();
-					alert(height);
 
 					$.ajax({
 						url:"shoeprocess.php",
 						type:"POST",
 						data:{
-							brand5:brand,
-							name4:name,
-							color3:color,
-							material2:material,
+							shoe_id1:shoe_id,
+							brand4:brand,
 							box:box,
 							length1:length,
 							width1:width,
@@ -285,120 +269,85 @@
 						success:function(divStr2){	
 							alert(divStr2);
 						},
-						error: function (divStr2) {
+						error: function () {
 							alert("Local error callback.");
-						},
-						complete: function (divStr1) {
-							//alert("Local completion callback.");
 						}
 					});
 				}
 			});
 		
 		</script>
-		
-		
+
 <?php
-
-	if(isset($_POST['upload'])){
-		
-		$filename = $_FILES['image']['name'];
-		$shoe_id = $_POST['shoe_id'];
-		$image_id = 1;
-		
-		$search_image_id = $mysqli->query("SELECT photo.image_id AS iid FROM photo WHERE photo.shoe_id = '$shoe_id';") or die($mysqli->error);
-		while($data6=mysqli_fetch_assoc($search_image_id)){
-			$image_id=$data6['iid'] + 1;
-		}
-		
-	   // Getting file name
-	   $filename = $_FILES['image']['name'];
-		if($filename != ""){
-		   // Valid extension
-		   $valid_ext = array('png','jpeg','jpg');
-				
-		   $photoExt1 = @end(explode('.', $filename)); // explode the image name to get the extension
-		   $phototest1 = strtolower($photoExt1);
-				
-		   $new_profle_pic = time().'.'.$phototest1;
-				
-		   // Location
-		   $location = "image/".$new_profle_pic;
-
-		   // file extension
-		   $file_extension = pathinfo($location, PATHINFO_EXTENSION);
-		   $file_extension = strtolower($file_extension);
-
-		   // Check extension
-		   if(in_array($file_extension,$valid_ext)){  
-
-				// Compress Image
-				compressedImage($_FILES['image']['tmp_name'],$location,60);
-					
-			}
-			else
-			{
-					echo "File format is not correct.";
-			}
-
-			 $sql = "INSERT INTO photo(shoe_id, image_id, image) VALUES ('" . $shoe_id . "', '" . $image_id . "', '".$new_profle_pic."')";
-
-			if (mysqli_query($mysqli, $sql)) 
-			{
-				echo "New record created successfully";
-				$divStr2 = "
-					<div>
-						<h3> Select File To Upload </h3>
-						<form method='post' action='' enctype='multipart/form-data'>
-							<input type='file' name='image' ><br>
-							<input type=\"hidden\" id=\"shoe_id\" name=\"shoe_id\" value=\"". $shoe_id ."\">
-							<input type='submit' value='Upload Image' name='upload'>  
-						</form>
-					<div class=\"row\">
-					<table border=\"2\">
-						<tr>
-							<td>No.</td>
-							<td>Images</td>
-						</tr>";
-					$no = 1;
-					$records = mysqli_query($mysqli,"select * from photo WHERE photo.shoe_id = $shoe_id "); 
-					while($data3 = mysqli_fetch_array($records)){
-						$path = "image/" . $data3['image'];
-						
-						$photo = "<img style=\"max-height: 400px ;max-width: 500px\" src= '" . $path . "' >";
-						$divStr2 = $divStr2 . "<tr><td>" . $no . "</td><td>" . $photo . "</td></tr>";
-						$no++;
-					}
-					$divStr2 = $divStr2 . "</table></div>";
-				}
-		} else {
-			
-		}
-	}	
-
-	// Compress image
-	function compressedImage($source, $path, $quality) {
-
-			$info = getimagesize($source);
-
-			if ($info['mime'] == 'image/jpeg') 
-				$image = imagecreatefromjpeg($source);
-
-			elseif ($info['mime'] == 'image/gif') 
-				$image = imagecreatefromgif($source);
-
-			elseif ($info['mime'] == 'image/png') 
-				$image = imagecreatefrompng($source);
-
-			imagejpeg($image, $path, $quality);
-
+if(isset($_POST['upload'])){
+	$filename = $_FILES['image']['name'];
+	$shoe_id = $_POST['shoe_id'];
+	
+	// Getting file name
+    $filename = $_FILES['image']['name'];
+   
+	$search_image_id = $mysqli->query("SELECT photo.image_id AS iid FROM photo WHERE photo.shoe_id = '$shoe_id';") or die($mysqli->error);
+	while($data6=mysqli_fetch_assoc($search_image_id)){
+		$image_id=$data6['iid'] + 1;
 	}
+	
+   // Getting file name
+   //$filename = $_FILES['image']['name'];
+	if($filename != ""){
+	   // Valid extension
+	   $valid_ext = array('png','jpeg','jpg');
+			
+	   $photoExt1 = @end(explode('.', $filename)); // explode the image name to get the extension
+	   $phototest1 = strtolower($photoExt1);
+			
+	   $new_profle_pic = time().'.'.$phototest1;
+			
+	   // Location
+	   $location = "image/".$new_profle_pic;
 
-	?>
-		
+	   // file extension
+	   $file_extension = pathinfo($location, PATHINFO_EXTENSION);
+	   $file_extension = strtolower($file_extension);
 
-		
-		
-		
+	   // Check extension
+	   if(in_array($file_extension,$valid_ext)){  
+
+			// Compress Image
+			compressedImage($_FILES['image']['tmp_name'],$location,60);
+				
+		}
+		else
+		{
+				echo "File format is not correct.";
+		}
+
+		$sql = "INSERT INTO photo(shoe_id, image_id, image)VALUES ('" . $shoe_id . "', '" . $image_id. "', '".$new_profle_pic."')";
+		if (mysqli_query($mysqli, $sql)) 
+		{
+			echo "New record created successfully";
+		}
+	} else {
+		echo "PLEASE SELECTED A FILE";
+	}
+}	
+
+// Compress image
+function compressedImage($source, $path, $quality) {
+
+		$info = getimagesize($source);
+
+		if ($info['mime'] == 'image/jpeg') 
+			$image = imagecreatefromjpeg($source);
+
+		elseif ($info['mime'] == 'image/gif') 
+			$image = imagecreatefromgif($source);
+
+		elseif ($info['mime'] == 'image/png') 
+			$image = imagecreatefrompng($source);
+
+		imagejpeg($image, $path, $quality);
+
+}
+?>		
     </body>
 </html>
